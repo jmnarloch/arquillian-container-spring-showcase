29a8cb8a869258b5cfbd51fb53a9c5d35f23e746
Jakub Narloch <jmnarloch@gmail.com>
2012-04-05 20:50:10 +0200

    Initial project structure.

---
+++ arquillian-spring-javaconfig/src/main/java/org/jboss/arquillian/showcase/App.java
@@ -0,0 +1,13 @@
+package org.jboss.arquillian.showcase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
